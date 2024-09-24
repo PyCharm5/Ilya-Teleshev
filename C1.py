1573f567226010bdc38ce7783b79c0fb4b0464eb
PyCharm5 <ilatelesev2@gmail.com>
2024-09-24 11:21:34 +0700

    Update C1.py

--- C1.py
+++ C1.py
@@ -1,5 +1,24 @@
-kb = int(input("Введите число КБ: "))
+type = input("Введите тип данных(гб, мб, кб): ")
 
-mb, byte, bit = kb // 1024, kb * 1024, kb * 8192
+try:
+    num = int(input())
+except:
+    print("Введите число")
+    exit()
 
-print(f'МБ-{mb}, Байты-{byte}, Бит-{bit}')
+if type == "гб":
+    mb = num * 1024
+    kb = mb * 1024
+    byte = kb * 1024
+    bit = byte * 8
+    print(f'МБ - {mb},КБ - {kb},Байт - {byte},Бит - {bit}')
+elif type == "мб":
+    kb = num * 1024
+    byte = kb * 1024
+    bit = byte * 8
+    print(f'КБ - {kb},Байт - {byte},Бит - {bit}')
+else:
+    mb = num // 1024
+    byte = num * 1024
+    bit = byte * 8
+    print(f'МБ - {mb},Байт - {byte},Бит - {bit}')
